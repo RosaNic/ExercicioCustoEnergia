2299f063ece2b7152edb264f06dd6e021d2a0073
Nicole <rosanik3103@gmail.com>
2023-09-29 15:44:21 -0300

    adciona exercicio

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Double Consumo, Diario;
+
+Decimal Custo, Resultado;
+
+Console.WriteLine
+    ("--- Custo de Energia ---");
+
+Console.Write
+    ("Consumo do aparelho por mês em kWh...: ");
+Consumo = Convert.ToDouble(Console.ReadLine());
+
+Console.Write
+    ("Horas de uso por dia.................: ");
+Diario = Convert.ToDouble(Console.ReadLine());
+
+Console.Write
+    ("Custo da energia em R$...............: ");
+Custo = Convert.ToDecimal(Console.ReadLine());
+
+Resultado = Convert.ToDecimal(Consumo * Diario) * Custo;
+
+Console.WriteLine
+    ($"Custo estimado: R$ " + Resultado);
